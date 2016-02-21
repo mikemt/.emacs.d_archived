f28ad21f8331d7e5d2c591a7728cc11d82a520c0
Michael Morris-Thomas <michael.morristhomas@gmail.com>
2016-02-21 09:17:15 +0800

    something is up with the exec-path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,8 +8,8 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (add-to-list 'load-path "~/.emacs.d/packages/")
 
-(if (display-graphic-p)
-    (set-exec-path-from-shell-PATH))
+;;(if (display-graphic-p)
+  ;;  (set-exec-path-from-shell-PATH))
 
 (setq visible-bell 1)
 (add-hook 'term-exec-hook 'set-no-process-query-on-exit)
