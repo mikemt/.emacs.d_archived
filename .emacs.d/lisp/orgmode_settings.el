d6eb970bac5acea7ec57b8baa38bbfdc46e896ff
Michael Morris-Thomas <michael.morristhomas@gmail.com>
2016-04-02 14:03:26 +0800

    Updated orgmode to turn on for *.txt files

--- .emacs.d/lisp/orgmode_settings.el
+++ .emacs.d/lisp/orgmode_settings.el
@@ -3,9 +3,8 @@
 ;; ----------------------------------------------------------------------
 (provide 'orgmode_settings)
 
-
-;; the following lines are always needed.  choose your own keys.
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
