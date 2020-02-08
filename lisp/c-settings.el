749115cd58c6ca3f53a5b270ea455b2240c56e92
Michael Morris-Thomas <michael.morristhomas@gmail.com>
2020-02-08 18:50:23 +0800

    Create c-settings.el

---
+++ lisp/c-settings.el
@@ -0,0 +1,18 @@
+;; ----------------------------------------------------------------------
+;; C/C++ settings
+;; ----------------------------------------------------------------------
+
+(provide 'c-settings)
+
+
+(defun local:local:ac-c-header-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'achead:include-directories '"/Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include")
+  )
+
+;; now let's call this function from c/c++ hooks
+(add-hook 'c++-mode-hook 'my:ac-c-header-init)
+(add-hook 'c-mode-hook 'my:ac-c-header-init)
+
+
