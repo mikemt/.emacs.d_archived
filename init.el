b55274ae8af72394597122e0248aa2ac5207ba8b
michael.morristhomas <michael.morristhomas@gmail.com>
2020-02-15 16:42:44 +0800

    loading cygwin settings

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 ;; ---------------------------------------------------------------------- 
 ;; Preliminaries
 ;; ---------------------------------------------------------------------- 
+(setq gc-cons-threshold 100000000)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
@@ -14,7 +16,7 @@
 (require 'custom_functions)
 (require 'server)
 
-(if (system-is-windows)
+(when (system-is-windows)
     (server-start))
 
 (when (system-is-darwin)
@@ -34,11 +36,20 @@
 ))
 
 (use-package powerline
-  :disabled t
+;;  :disabled t
   :ensure t
   :config
   (powerline-default-theme)
+  (setq column-number-indicator-zero-based nil)
   )
+
+(use-package cygwin-mount
+  :config
+  (setq cygwin-mount-cygwin-bin-directory "c:/cygwin64/bin")
+  (cygwin-mount-activate))
+
+
+(use-package setup-cygwin)
 
 
 ;; ---------------------------------------------------------------------- 
@@ -62,6 +73,13 @@
 ;; Appearance customizations
 ;; ---------------------------------------------------------------------- 
 
+(when (system-is-windows)
+  (set-face-attribute 'default nil
+                      :family "MesloLGS NF"
+                      :height 90
+                      :weight 'normal
+                      :width 'normal)
+)
 
 (when (system-is-darwin)
    (add-to-list 'default-frame-alist '(font . "Monaco 11"))
@@ -70,7 +88,26 @@
 
 (put 'downcase-region 'disabled nil)
 
+(defun display-startup-echo-area-message ()
+  (message (emacs-init-time)))
 
 
 
 
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zerodark-theme yasnippet which-key use-package powerline org-ref material-theme leuven-theme jedi ivy-rich ido-vertical-mode ein doom-themes doom-modeline cython-mode counsel auctex ample-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
